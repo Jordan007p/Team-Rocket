2dbc3996707339ee59068d0d9f6e027835f3ba33
jackspell <m.metodij@outlook.com>
2022-01-31 20:26:15 +0100

    Added Domasna4

---
+++ Domasna4/controller.js
@@ -0,0 +1,159 @@
+const express = require('express')
+const app = express()
+const port = 80
+const path = require('path');
+const bodyParser = require('body-parser');
+const router = express.Router();
+const {spawn} = require('child_process');
+const fs = require('fs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const options = {
+        root: path.join(__dirname)
+    };
+    res.sendFile('index.html', options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent index.html to '+ip);
+        }
+    });
+});
+
+app.get('/Alata-Regular.ttf', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const options = {
+        root: path.join(__dirname)
+    };
+    res.sendFile('Alata-Regular.ttf', options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent Alata-Regular.ttf to '+ip);
+        }
+    });
+});
+
+app.get('/map.html', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const options = {
+        root: path.join(__dirname)
+    };
+    res.sendFile('map.html', options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent map.html to '+ip);
+        }
+    });
+});
+
+app.get('/map_template.html', (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const options = {
+        root: path.join(__dirname)
+    };
+    res.sendFile('map_template.html', options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent map_template.html to '+ip);
+        }
+    });
+});
+
+router.post('/result',(request,response) => {
+    //response.send('Response!');
+    var dataToSend;
+    const python = spawn('python', ['mapgen.py',request.body["from"],request.body["to"]]);
+    python.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ... '+data);
+        dataToSend = data.toString();
+    });
+    python.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+        if(code==0){
+            
+            var distandur = dataToSend.split('\n');
+            var distance = (parseFloat(distandur[0].split(',')[0].split(': ')[1])/1000).toFixed(2);
+            var duration = parseInt(parseFloat(distandur[0].split(',')[1].split(': ')[1])/60);
+            var pat_distance = (parseFloat(distandur[1].split(',')[0].split(': ')[1])/1000).toFixed(2);
+            var pat_duration = parseInt(parseFloat(distandur[1].split(',')[1].split(': ')[1])/60);
+            
+            const prosecna_potrosuvacka = 0.07;
+            const cena_na_benzin = 	78.5;
+            const toll_price = parseFloat(120);
+
+            var gorivo = parseFloat(distance*prosecna_potrosuvacka*cena_na_benzin).toFixed(1);
+            var pat_gorivo = parseFloat(pat_distance*prosecna_potrosuvacka*cena_na_benzin).toFixed(1);
+
+            var full_price = parseFloat(toll_price)+parseFloat(gorivo);
+            //console.log(full_price);
+
+            var distance_diff = Math.abs(distance-pat_distance).toFixed(2);
+            var duration_diff = Math.abs(duration-pat_duration);
+            var gas_diff = Math.abs(full_price-pat_gorivo).toFixed(2);
+
+            var more_distant = (distance>pat_distance)? '(Patarina Pat is more distant)' : '(Non-patarina pat is more distant)';
+            var more_durr = (duration>pat_duration)? '(Patarina Pat takes longer)' : '(Non-patarina pat takes longer)';
+            var more_pricy = (full_price>pat_gorivo)? '(Patarina Pat costs more)' : '(Non-patarina pat costs more)';
+            
+            //console.log('dalecina: '+distance+' - vreme: '+duration);
+            //console.log('pat_dalecina: '+pat_distance+' - pat_vrmee: '+pat_duration);
+
+            var data = fs.readFileSync('result_template.html', 'utf-8');
+
+            var newValue = data.replace('<div id="box1_1"></div>','<div id="box1_1">Length in km: '+distance+'<br>Time needed in min: '+duration+'<br>\
+            Gas price: '+gorivo+' MKD<br>Toll price: '+toll_price+' MKD<br></div>');
+            var newValue1 = newValue.replace('<div id="box1_2"></div>','<div id="box1_2">Length in km: '+pat_distance+'<br>Time needed in min: '+pat_duration+'<br>\
+            Gas price: '+pat_gorivo+' MKD<br></div>');
+            newValue = newValue1.replace('document.getElementById("from").value = "";','document.getElementById("from").value = "'+request.body["from"]+'";');
+            newValue1 = newValue.replace('document.getElementById("to").value = "";','document.getElementById("to").value = "'+request.body["to"]+'";');
+            newValue = newValue1.replace('<div id="box3_1"></div>','<div id="box3_1">Difference in length: '+distance_diff+' km '+more_distant+'<br>Difference in time: \
+            '+duration_diff+' min '+more_durr+'<br>Difference in price: '+gas_diff+' MKD '+more_pricy+'<br></div>');
+
+
+            fs.writeFileSync('result.html', newValue, 'utf-8');
+
+            console.log('readFileSync complete');
+
+
+            //console.log(request.body);
+
+            const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+            const options = {
+                root: path.join(__dirname)
+            };
+            response.sendFile('result.html', options, function (err) {
+                if (err) {
+                    next(err);
+                } else {
+                    console.log('Sent result.html to '+ip);
+                }
+            });
+        }
+        else{
+            const ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress;
+            const options = {
+                root: path.join(__dirname)
+            };
+            response.sendFile('error.html', options, function (err) {
+                if (err) {
+                    next(err);
+                } else {
+                    console.log('Sent error.html to '+ip);
+                }
+            });
+        }
+    });
+});
+
+app.use('/',router);
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
---
+++ Domasna4/mapgen.py
@@ -0,0 +1,81 @@
+import shapely
+import folium
+import openrouteservice as ors
+import json
+import sqlite3
+import sys
+
+from shapely import geometry
+from shapely.geometry import MultiPolygon, Polygon
+
+#coordinates = [[21.43165, 41.99609], [22.50217, 41.14762]]  # OVDE STAVI POCETNI I KRAJNI KOORDINATI
+
+def generate_map(coordinates):
+    ors_key = '5b3ce3597851110001cf62488658392d825546aeaf8199344e027b60'
+
+    client = ors.Client(key=ors_key)
+
+    f = open('patarini.json')
+    polygons = json.load(f)
+
+    avoid_polygons = []
+
+    for data in polygons['features']:
+        cords = data['geometry']['coordinates']
+        shell = [cords[0][0], cords[0][1], cords[0][2], cords[0][3]]
+        poly = Polygon(shell)
+        avoid_polygons.append(poly)
+
+    multipolygon = MultiPolygon(avoid_polygons)
+
+    avoid_options = {
+
+        'avoid_polygons': geometry.mapping(multipolygon)
+    }
+
+    route = client.directions(coordinates=coordinates,
+                            profile='driving-car',
+                            format='geojson')
+
+    route1 = client.directions(coordinates=coordinates,
+                            profile='driving-car',
+                            format='geojson', options=avoid_options)
+
+    print(route['features'][0]["properties"]["summary"])  # OVA E TOA STO TI TREBA DISTANCE E VO METRI I DURATION E VO SEKUNDI
+    print(route1['features'][0]["properties"]["summary"])
+    #print(route['features'])
+    # map
+    map_directions = folium.Map(location=[41.99609, 21.43165], zoom_start=8.5)
+
+    # add geojson to map
+    folium.GeoJson(route, name='route').add_to(map_directions)
+    folium.GeoJson(route1, name='route1').add_to(map_directions)
+
+    # add layer control to map (allows layer to be turned on or off)
+    folium.LayerControl().add_to(map_directions)
+    # display map
+    map_directions
+
+    map_directions.save('map.html')
+
+#generate_map(coordinates)
+
+#from_to_file = open("from_to.json")
+#from_to = json.load(from_to_file)
+#fromm = from_to["from"]
+#to = from_to["to"]
+
+fromm = sys.argv[1]
+to = sys.argv[2]
+conn = sqlite3.connect("baza.db")
+command = ("select lat from gradovi where ime like \""+fromm+"\";")
+startlat = conn.execute(command).fetchall()[0][0]
+command = ("select lon from gradovi where ime like \""+fromm+"\";")
+startlon = conn.execute(command).fetchall()[0][0]
+command = ("select lat from gradovi where ime like \""+to+"\";")
+destlat = conn.execute(command).fetchall()[0][0]
+command = ("select lon from gradovi where ime like \""+to+"\";")
+deslon = conn.execute(command).fetchall()[0][0]
+conn.close()
+coor = [[startlon,startlat],[deslon,destlat]]
+generate_map(coor)
